8b6d253750fc59b53428bad133b6dd5927b07322
ZakryZ <zakizoukruf@gmail.com>
2018-10-18 01:20:07 +0700

    Chapter 8 Basic Login done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 include ApplicationHelper
   protect_from_forgery with: :exception
-
+  include SessionsHelper
   def hello
     render html: "hello, world!"
   end
