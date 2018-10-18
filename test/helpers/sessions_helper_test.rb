f82570ad6bba8283434dbabd99526d4eacaca33e
ZakryZ <zakizoukruf@gmail.com>
2018-10-19 00:01:57 +0700

    Chapter 11 adding account activation,mailers

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -1,24 +1,10 @@
 require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
-
   def setup
     @user = users(:zakryz)
     remember(@user)
   end
-
-  # Returns the user corresponding to the remember token cookie.
-   def current_user
-     if (user_id = session[:user_id])
-       @current_user ||= User.find_by(id: user_id)
-     elsif (user_id = cookies.signed[:user_id])
-       user = User.find_by(id: user_id)
-       if user && user.authenticated?(cookies[:remember_token])
-         log_in user
-         @current_user = user
-       end
-     end
-   end
 
   test "current_user returns right user when session is nil" do
     assert_equal @user, current_user
