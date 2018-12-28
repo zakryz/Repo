c2719d3a3cbcce40ceef6e610c9847696d7a1f0e
ZakryZ <zakizoukruf@gmail.com>
2018-12-29 00:33:59 +0700

    Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
    :address        => 'smtp.sendgrid.net',
    :port           => '587',
    :authentication => :plain,
-   :user_name      => ENV['SENDGRID_USERNAME'],
-   :password       => ENV['SENDGRID_PASSWORD'],
+   :user_name      => ENV['app120342640@heroku.com'],
+   :password       => ENV['gtv1scud3177'],
    :domain         => 'heroku.com',
    :enable_starttls_auto => true
  }
